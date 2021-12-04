f2ee5e5bab09342cd2c50af2d332a9cb92fda0c4
Eliaris Alvares <85964972+eliarisalvares@users.noreply.github.com>
2021-12-04 14:27:18 -0300

    Final commit

--- sources/ft_create_image.c
+++ sources/ft_create_image.c
@@ -6,7 +6,7 @@
 /*   By: elraira- <elraira-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/26 13:51:22 by elraira-          #+#    #+#             */
-/*   Updated: 2021/12/02 16:18:04 by elraira-         ###   ########.fr       */
+/*   Updated: 2021/12/03 16:47:56 by elraira-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,13 @@
 
 void	ft_draw_pixels(int x, int y, int color, t_fractol *ptr)
 {
-	int	index;
+	int	i;
 	int	*ptr2;
 
 	if (x >= 0 && x < ptr->window_width && y >= 0 && y < ptr->window_height)
 	{
-		index = y * ptr->img->line_length + x * ptr->img->bits_per_pixel / 8;
-		ptr2 = (int *)&ptr->img->data[index];
+		i = y * ptr->img->line_length + x * ptr->img->bits_per_pixel / 8;
+		ptr2 = (int *)&ptr->img->data[i];
 		*ptr2 = color;
 	}
 }
@@ -42,4 +42,14 @@ int	ft_put_image_to_window(t_fractol *ptr)
 	mlx_put_image_to_window(ptr->mlx_ptr,
 		ptr->mlx_win, ptr->img->mlx_img, 0, 0);
 	return (0);
+}
+
+void	ft_launch_each_fractal(t_fractol *ptr)
+{
+	if (ptr->fractal_name == 1)
+		ft_calculate_and_launch_mandelbrot(ptr);
+	else if (ptr->fractal_name == 2)
+		ft_calculate_and_launch_julia(ptr);
+	else if (ptr->fractal_name == 3)
+		ft_calculate_and_launch_burning_ship(ptr);
 }
