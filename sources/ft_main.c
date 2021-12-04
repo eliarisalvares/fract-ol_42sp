e9cf429d8688c878137d5109c019ec5692131adf
Eliaris Alvares <85964972+eliarisalvares@users.noreply.github.com>
2021-12-04 14:33:18 -0300

    Update ft_main.c

--- sources/ft_main.c
+++ sources/ft_main.c
@@ -6,13 +6,9 @@
 /*   By: elraira- <elraira-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/26 13:51:22 by elraira-          #+#    #+#             */
-/*   Updated: 2021/12/02 18:59:44 by elraira-         ###   ########.fr       */
+/*   Updated: 2021/12/03 13:52:51 by elraira-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-/* Main function check and validates arguments and allocates memory for the 
-structs. If allocation fails, an error message is displayed. Also, here we 
-define window configurations, such as height, width and display shift. */
 
 #include "../includes/ft_fractol.h"
 
@@ -31,9 +27,7 @@ int	main(int argc, char **argv)
 	ptr->window_height = 600;
 	ptr->display_shift = 0.1;
 	if (argc < 2 || argc > 4 || ft_check_fractal_name(ptr, argv, argc) != 1)
-	{
 		ft_usage_message();
-	}
 	else
 		ft_init_hooks_and_loop(ptr);
 	return (0);
@@ -64,14 +58,4 @@ void	ft_init_each_fractal_settings(t_fractol *ptr)
 		ft_julia_settings(ptr);
 	else if (ptr->fractal_name == 3)
 		ft_burning_ship_settings(ptr);
-}
-
-void	ft_launch_each_fractal(t_fractol *ptr)
-{
-	if (ptr->fractal_name == 1)
-		ft_calculate_and_launch_mandelbrot(ptr);
-	else if (ptr->fractal_name == 2)
-		ft_calculate_and_launch_julia(ptr);
-	else if (ptr->fractal_name == 3)
-		ft_calculate_and_launch_burning_ship(ptr);
 }
