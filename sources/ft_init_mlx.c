69f39451e86b1aae9c61188068b6b026a1e41cf2
Eliaris Alvares <85964972+eliarisalvares@users.noreply.github.com>
2021-12-04 14:32:34 -0300

    Update ft_init_mlx.c

--- sources/ft_init_mlx.c
+++ sources/ft_init_mlx.c
@@ -1,16 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_fractol.c                                       :+:      :+:    :+:   */
+/*   ft_init_mlx.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: elraira- <elraira-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/26 13:51:22 by elraira-          #+#    #+#             */
-/*   Updated: 2021/12/02 15:40:13 by elraira-         ###   ########.fr       */
+/*   Updated: 2021/12/03 13:44:14 by elraira-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_fractol.h"
+
+int	ft_init_hooks_and_loop(t_fractol *ptr)
+{
+	ft_init_mlx(ptr);
+	mlx_hook(ptr->mlx_win, 2, 1L << 0, ft_key_hook, ptr);
+	mlx_mouse_hook(ptr->mlx_win, ft_zoom_hook, ptr);
+	mlx_loop(ptr->mlx_ptr);
+	return (0);
+}
 
 int	ft_init_mlx(t_fractol *ptr)
 {
